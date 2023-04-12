16e8cf31103e67ba552a1ec6ea1897bffc66a0a1
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2023-04-12 09:54:38 +0200

    new

--- script/script.js
+++ script/script.js
@@ -1,49 +1,6 @@
-let playButton = document.querySelector("main > section:first-of-type button:first-of-type")
-
-let refreshButton = document.querySelector("main > section:first-of-type button:last-of-type")
-
-let tonesList = document.querySelector("header ul")
-let tones = document.querySelectorAll("header ul li")
-let pianoButtons = document.querySelectorAll("main ul li > div button")
-
-let plusButton = document.querySelector("main > button")
 let pianoList = document.querySelector("main > ul")
+let plusButton = document.querySelector("main > button")
 
-playButton.addEventListener("click", () => {
-    playButton.classList.toggle("play");
-    console.log("play");
-    tonesList.classList.toggle("playTone");
-})
-
-
-refreshButton.addEventListener("click", () => {
-    playButton.classList.remove("play");
-    tonesList.classList.remove("playTone");
-
-    for (let i = 0; i < pianoButtons.length; i++) {
-        pianoButtons[i].classList.remove("active");
-    }
-})
-
-
-for (let i = 0; i < pianoButtons.length; i++) {
-    pianoButtons[i].addEventListener("click", () => {
-        pianoButtons[i].classList.toggle("active");
-        console.log("yes", [i]);
-
-        if (pianoButtons[i] == pianoButtons[11]) {
-            console.log("11");
-            tones[12].classList.toggle("tone");
-        }
-        else if (pianoButtons[i] == pianoButtons[15]) {
-            console.log("15", tones[16]);
-            tones[16].classList.toggle("tone");
-        }
-        else {
-            tones[i].classList.toggle("tone");
-        }
-    })
-}
 
 plusButton.addEventListener("click", () => {
     let liElement = document.createElement("li")
@@ -85,7 +42,54 @@ plusButton.addEventListener("click", () => {
 })
 
 
+
+let playButton = document.querySelector("main > section:first-of-type button:first-of-type")
+let refreshButton = document.querySelector("main > section:first-of-type button:last-of-type")
 let deleteButtons = document.querySelectorAll("main ul li > button")
+
+let tonesList = document.querySelector("header ul")
+let tones = document.querySelectorAll("header ul li")
+let pianoButtons = document.querySelectorAll("main ul li > div button")
+
+
+playButton.addEventListener("click", () => {
+    playButton.classList.toggle("play");
+    console.log("play");
+    tonesList.classList.toggle("playTone");
+})
+
+
+refreshButton.addEventListener("click", () => {
+    playButton.classList.remove("play");
+    tonesList.classList.remove("playTone");
+
+    for (let i = 0; i < pianoButtons.length; i++) {
+        pianoButtons[i].classList.remove("active");
+        tones[i].classList.remove("tone");
+    }
+})
+
+
+for (let i = 0; i < pianoButtons.length; i++) {
+    pianoButtons[i].addEventListener("click", () => {
+        pianoButtons[i].classList.toggle("active");
+        console.log("tone", [i]);
+
+        if (pianoButtons[i] == pianoButtons[1]) {
+            tones[1].classList.toggle("tone");
+            tones[12].classList.toggle("tone");
+
+        }
+        else if (pianoButtons[i] == pianoButtons[5]) {
+            tones[5].classList.toggle("tone");
+            tones[16].classList.toggle("tone");
+
+        }
+        else {
+            tones[i].classList.toggle("tone");
+        }
+    })
+}
 
 for (let i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].addEventListener("click", () => {
